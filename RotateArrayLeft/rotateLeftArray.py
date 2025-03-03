0d164fee79e7c394bc8ce8f0620f435708d9cf46
Victor Kim <victorkim@hotmail.ca>
2025-03-03 16:45:23 -0500

    add solution for rotate array left

---
+++ RotateArrayLeft/rotateLeftArray.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, arr):
+    # Write your code here
+
+    # this code is still O(n^2)
+    # a bit faster than the one manually performing the shifting of all elements
+    # because built in functions allows convertion to C code 
+    for i in range(d):
+        front = arr.pop(0)
+        arr.append(front)
+        
+    return arr
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
